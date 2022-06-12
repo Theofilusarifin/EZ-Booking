c3c7a7589e4971f3521c3aa67bbfb06239f43a89
Valencia <s160820016@student.ubaya.ac.id>
2022-06-12 13:56:15 +0700

    Update LoginForm.java

--- client_gui/src/auth/LoginForm.java
+++ client_gui/src/auth/LoginForm.java
@@ -27,17 +27,28 @@ public class LoginForm extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        lblForm = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        lblForm.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        lblForm.setText("FORM LOG-IN");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(136, 136, 136)
+                .addComponent(lblForm, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(155, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(22, 22, 22)
+                .addComponent(lblForm)
+                .addContainerGap(261, Short.MAX_VALUE))
         );
 
         pack();
@@ -79,5 +90,6 @@ public class LoginForm extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel lblForm;
     // End of variables declaration//GEN-END:variables
 }
