8ed7ddcbf825a7bdc7ce854cd173630d606bfa26
jabeshnehemiah <jabesh.nehemiah@gmail.com>
2022-06-16 18:18:51 +0700

    Create Menu.java

---
+++ server_gui/src/model/Menu.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author jabeshnehemiah
+ */
+public class Menu extends MyConnection{
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getRestaurant_id() {
+        return restaurant_id;
+    }
+
+    public void setRestaurant_id(int restaurant_id) {
+        this.restaurant_id = restaurant_id;
+    }
+    private int id;
+    private String name;
+    private double price;
+    private int restaurant_id;
+}
